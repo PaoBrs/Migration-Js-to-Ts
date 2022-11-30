d637d06500ab9c41e6d975e1fba7b3899e429055
Paola Barbosa <p.barbosa93@gmail.com>
2022-11-29 22:28:35 -0500

    added types to GameStateBar

--- TicTacToe_JS/src/gameStateBar.tsx
+++ TicTacToe_JS/src/gameStateBar.tsx
@@ -5,29 +5,29 @@ interface GameStateBarState {
     gameState: GameState
 }
 
-export class GameStateBar extends React.Component {
+export class GameStateBar extends React.Component<{}, GameStateBarState> {
     
-    constructor(props) {
+    constructor(props:{}) {
         super(props);
         this.state = {gameState: ""};
     }
     
-    handleGameStateChange(e) {
+    handleGameStateChange(e:CustomEvent) {
         this.setState({gameState: e.detail});
     }
   
-    handleRestart(e) {
+    handleRestart(e:Event) {
         this.setState({gameState: ""});
     }
 
     componentDidMount() {
-        window.addEventListener("gameStateChange", e => this.handleGameStateChange(e));
-        window.addEventListener("restart", e => this.handleRestart(e));
+        window.addEventListener("gameStateChange", (e:CustomEvent) => this.handleGameStateChange(e));
+        window.addEventListener("restart", (e:CustomEvent) => this.handleRestart(e));
     }
 
     componentWillUnmount() {
-        window.removeEventListener("gameStateChange", e => this.handleGameStateChange(e));
-        window.removeEventListener("restart", e => this.handleRestart(e));
+        window.removeEventListener("gameStateChange", (e:CustomEvent) => this.handleGameStateChange(e));
+        window.removeEventListener("restart", (e:CustomEvent) => this.handleRestart(e));
     }
     
     render() {
